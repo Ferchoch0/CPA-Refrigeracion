a4f2f68ad9c7ca89963073940c44c8beba0e909d
Ferchoch0 <delvalle.fernando.d@gmail.com>
2025-09-23 15:45:34 -0300

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Icon from "react-native-vector-icons/Ionicons";
 
 import HomeScreen from "../pages/Home"; // Tu pantalla real de Home
-import EquiposScreen from "../pages/Equipos"; 
-import GeneralScreen from "../pages/FormGeneral";
 import ProfileScreen from "../pages/Profile";
+import QRScanner from "../pages/QrScanner"; // Pantalla del lector de QR
 
 const Tab = createBottomTabNavigator();
 
@@ -34,14 +33,14 @@ export default function Navbar() {
       {/* QR Code */}
       <Tab.Screen
         name="QR"
-        component={HomeScreen} // Cambiar después a pantalla QR
+        component={QRScanner} // ahora abre el lector
         options={{
           tabBarIcon: () => (
             <View style={styles.centerButton}>
               <Icon name="qr-code" size={28} color="#fff" />
             </View>
           ),
-          tabBarLabel: "", // Oculta el texto debajo del botón QR
+          tabBarLabel: "",
         }}
       />
 
